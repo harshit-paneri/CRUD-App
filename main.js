0d7b73a6a01984759bd8c55e7d7812c25dde6719
Harshit <harshitpanari@gmail.com>
2023-08-17 01:20:04 +0530

    crud app creation : models schema created

---
+++ main.js
@@ -0,0 +1,45 @@
+// imports
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+// DB connection
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on("error", (error) => console.log(error));
+db.once("open", () => console.log("Connected to the database!"));
+
+//middlewears
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+app.use(
+  session({
+    secret: "my secre key",
+    saveUnitialized: true,
+    resave: false,
+  })
+);
+
+app.use((req,res,next)=>{
+    res.locals.message=req.session.message;
+    delete req.session.message;
+    next();
+});
+
+// set template engine
+app.set('view engine','ejs');
+
+
+
+app.get("/", (req, res) => {
+  res.send("Hello Word");
+});
+
+app.listen(PORT, () => {
+  console.log(`SERVER started at http://localhost:${PORT}`);
+});
