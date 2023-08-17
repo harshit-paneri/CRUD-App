40740ad19caff2f38be8cd0a5dc79f5bee6564b0
Harshit <harshitpanari@gmail.com>
2023-08-17 16:04:32 +0530

    EJS message and post request working

--- routes/routes.js
+++ routes/routes.js
@@ -1,5 +1,47 @@
 const express = require("express");
 const router = express.Router();
+const User = require("../models/users");
+const multer = require("multer");
+
+// img upload
+var storage= multer.diskStorage({
+    destination : function(req,file,cb){
+        cb(null,"./uploads");
+    },
+    filename: function(req, file, cb){
+        cb(null, file.filename+"_"+ Date.now()+"_"+file.originalname);
+    },
+});
+
+// like middleware
+
+var upload=multer({
+    storage: storage,
+}).single('image');
+
+// Insert an user into database route
+router.post('/add', upload, async (req, res) => {
+    try {
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email,
+            phone: req.body.phone,
+            image: req.file.filename,
+        });
+
+        await user.save(); // Use await to handle the asynchronous operation
+
+        req.session.message = {
+            type: 'success',
+            message: 'User added successfully!'
+        };
+        res.redirect('/');
+    } catch (err) {
+        res.json({ message: err.message, type: 'danger' });
+    }
+});
+
+
 
 //user route
 router.get("/users",(req,res)=>{
