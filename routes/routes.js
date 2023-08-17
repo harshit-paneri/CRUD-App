3339962a59eff97f8c20e70c5949654b234de3d4
Harshit <harshitpanari@gmail.com>
2023-08-17 12:23:03 +0530

    routes creation/ user add and view/layout

--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const router = express.Router();
 
+//user route
 router.get("/users",(req,res)=>{
     res.send("All Users");
+});
+//home route
+router.get("/",(req,res)=>{
+    // res.send("All Buddy you are at home");
+    res.render("index",{title : "Home Page"})
+});
+
+//add user route
+router.get("/add",(req,res)=>{
+    // res.send("All Buddy you are at home");
+    res.render("add_user",{title : "Add User"})
 });
 
 module.exports= router;
